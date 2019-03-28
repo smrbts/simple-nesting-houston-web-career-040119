531c7017066dc32cde8535eb2166d327b9c0ebac
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 19:15:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,8 +58,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-programmer_hash
-
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
